9fded72909da1bf61f5c867727e51adfdd8f4aa6
goracijCerv <100542978+goracijCerv@users.noreply.github.com>
2023-02-07 12:34:02 -0600

    al dia 07 de febrero
    swswsw

--- cliente/src/app/app.component.ts
+++ cliente/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cliente';
+export class AppComponent implements OnInit {
+  title = 'Mi Primer Cita';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response: any) => this.users = response,
+      error: (err: Error) => console.log(err),
+      complete: () => console.log('Request has completed')
+    });
+  }
 }
