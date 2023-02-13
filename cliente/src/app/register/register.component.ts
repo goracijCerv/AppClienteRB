d3272b68ae7177266cc92ebfef445a7589aabcf7
goracijCerv <100542978+goracijCerv@users.noreply.github.com>
2023-02-13 11:49:15 -0600

    Agregando el componente registro

---
+++ cliente/src/app/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  model: any ={};
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+  
+  register(): void {
+    console.log(this.model);
+  }
+
+  cancel(): void {
+    console.log("cancelld");
+  }
+
+}
