64f04228a657e5a61aa483ce4b5e969686f6ef87
goracijCerv <100542978+goracijCerv@users.noreply.github.com>
2023-03-06 12:46:27 -0600

    Cambios del 6 de marzo, parece que esta todo bien

--- cliente/src/app/members/member-edit/member-edit.component.ts
+++ cliente/src/app/members/member-edit/member-edit.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-member-edit',
   templateUrl: './member-edit.component.html',
@@ -12,10 +13,16 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 
 export class MemberEditComponent implements OnInit{
+ @ViewChild('editForm') editForm: NgForm | undefined 
+ @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any){
+  if(this.editForm?.dirty){
+    $event.returnValue = true;
+  }
+ }
  member: Member | undefined;
  user: User | null = null;
 
- constructor(private accountService: AccountService, private memberService: MembersService){
+ constructor(private accountService: AccountService, private memberService: MembersService, private toaster: ToastrService){
   this.accountService.currentUser$.pipe(take(1)).subscribe({
     next: user => this.user = user
   });
@@ -32,4 +39,9 @@ export class MemberEditComponent implements OnInit{
   });
  }
 
+ updateMember(){
+  console.log(this.member);
+  this.toaster.success('Profile updated')
+  this.editForm?.reset(this.member);
+ }
 }
